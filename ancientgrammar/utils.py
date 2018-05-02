f04df1ea9f35d55fa673ae6f7a0fcf17928f3e38
Joe <joe.bell34@btinternet.com>
2018-05-02 13:49:10 +0100

    Unicode fun eheh

--- ancientgrammar/utils.py
+++ ancientgrammar/utils.py
@@ -1,7 +1,15 @@
 import unicodedata
 def is_vowel(char):
-    return remove_accents(char) in "αειορυω"
+    return basic_char(char) in "αειορυω"
+
+def is_equal(string1:str, string2:str):
+    return remove_accents(string1) == remove_accents(string2)
+
+def basic_char(string:str):
+    return ''.join(c for c in unicodedata.normalize('NFD', string) 
+            if unicodedata.category(c) != 'Mn')
 
 def remove_accents(string:str):
-    return ''.join(c for c in unicodedata.normalize('NFD', s) 
-            if unicodedata.category(c) != 'Mn')
+    '''Keeps IOTA SUBSCRIPT'''
+    return unicodedata.normalize("NFC", ''.join(c for c in unicodedata.normalize('NFD', string) 
+            if unicodedata.category(c) != 'Mn' or c == "\u0345"))
